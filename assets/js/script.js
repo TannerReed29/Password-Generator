da3d77b6ccc1bba0af0be281e3e5ba840491b818
Tanner Brieger <tanner29@gmail.com>
2021-11-19 02:28:19 -0600

    created generate password function and started adding variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,53 @@
 // Assignment code here
+// 1. Prompt user for password criteria
+//   . password length 8-128
+//   . lowercase uppercase numeric special characters
+// 2. Validate the input of criteria
+// 3. Generate passsword based on validated criteria
 
+
+
+var lower = 'abcdefghijklmnopqrstuvwxyz'
+var upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' 
+var numbers = '0123456789'
+var characters = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
+
+function generatePassword(plength) {
+  var plength = window.prompt('input desired password length');
+  plength = parseInt(plength);
+  if (isNaN(plength))
+    {
+    alert("input is not a number");
+    }
+  else
+    {
+      if (plength >= 8 && plength <= 128)
+        {
+        console.log(plength);
+        }
+    }
+  
+
+
+  //var lowercase = window.prompt('Would you like to include lowercase');
+  //var uppercase = window.prompt('Would you like to include Uppercase');
+  //var numeric = window.prompt('Would you like to include numbers');
+  //var special = window.prompt('Would you like to include Special Charcters');
+  
+
+
+
+
+
+
+
+
+
+
+// return variable of generated password here
+  return "Password goes here";
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
